595b1c302920100ab495756751b8b6dc0a41cc65
Nouman Iftikhar <nouman72884@gmail.com>
2023-02-15 08:04:42 +0500

    added cognito details

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_T2ZAQQAkp', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '621k589b9kor0j4icjbgk5kr7v', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
